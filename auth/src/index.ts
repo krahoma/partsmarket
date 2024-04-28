50493b96fe0af4bee41ee301e70d821c97c449a5
Marian Krekhovetskyi <m.krekhovetskyi@astoundcommerce.com>
2024-04-28 18:24:42 +0300

    Small corection - remove log

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -9,7 +9,6 @@ const starUp = async () => {
   }
   try {
     await mongoose.connect(process.env.MONGO_URI!);
-    console.log("Connected to MongoDB");
   } catch (err) {
     console.log(err); 
   }
